a1863b4da72de56c7099f7bb7de0884b2c6c9dd9
Ville Vainio <ville.vainio@gmail.com>
2013-08-23 21:03:00 +0300

    retry

--- app.rb
+++ app.rb
@@ -9,20 +9,46 @@ highscore_lb = Leaderboard.new('highscores')
 highscore_lb.page_size = 10
 
 get '/leaders' do
- content_type :json
- highscore_lb.leaders(1).to_json
+  begin
+    content_type :json
+    highscore_lb.leaders(1).to_json
+  rescue
+    status 500
+  end 
 end
 
-post '/add' do  
-  if params[:nickname] and not params[:nickname].empty?  
-    highscore_lb.rank_member(params[:nickname], params[:score])
-    status 200
+get '/aroundme' do
+  if params[:nickname] and not params[:nickname].empty?
+    begin
+      content_type :json
+      nickname = params[:nickname].strip
+      highscore_lb.around_me(nickname, page_size: 5).to_json
+    rescue
+      status 500
+    end
   end
 end
 
-# debugging
-get '/reset' do
-  highscore_lb.delete_leaderboard
+post '/add' do  
+  if params[:nickname] and not params[:nickname].empty?
+    if params[:score] and not params[:score].empty?
+      begin
+        nickname = params[:nickname].strip
+        score = params[:score].to_i
+        
+        highscore_check = lambda do |member, current_score, score, member_data, leaderboard_options|
+          return true if current_score.nil?
+          return true if score > current_score
+          false
+        end
+
+        highscore_lb.rank_member_if(highscore_check, nickname, score)
+        status 200
+      rescue
+        status 500
+      end
+    end
+  end
 end
 
 not_found do  
